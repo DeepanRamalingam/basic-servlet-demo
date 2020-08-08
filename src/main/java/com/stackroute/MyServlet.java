4724c4f704e9d6e15944d59f82a2ec27ab639ab1
deepan.ramalingam <deepan.ramalingam@stackroute.in>
2020-08-08 15:46:27 +0530

    reference demo

---
+++ src/main/java/com/stackroute/MyServlet.java
@@ -0,0 +1,30 @@
+package com.stackroute;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println(req.getRequestURI());
+//		PrintWriter out = resp.getWriter();
+//		resp.setContentType("text/html");
+//		String message = "Welcome to my page";
+//		out.println("<h1>" + message + "</h1>");
+		String message = "Deepan Chakkaravarthy";
+		req.setAttribute("msg", message);
+		RequestDispatcher rd = req.getRequestDispatcher("/myhome");
+		rd.forward(req, resp);
+		
+		
+	}
+
+	
+}
